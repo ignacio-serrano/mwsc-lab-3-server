dac9b1b800bd682ebbf33fe0a0cfab4538fb078c
Ignacio Serrano Zabaleta <ignacio.serrano@gft.com>
2017-04-30 17:44:26 +0200

    Project import.

---
+++ src/main/java/com/gft/ioso/lab3/server/Lab3ServerApplication.java
@@ -0,0 +1,12 @@
+package com.gft.ioso.lab3.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3ServerApplication.class, args);
+	}
+}
